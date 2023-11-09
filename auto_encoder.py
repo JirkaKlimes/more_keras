49a2a98508166780bc23a80d429cdfbabd4797fc
JirkaKlimes <jiriklimes920@gmail.com>
2023-11-09 18:00:53 +0100

    stripped SE_block

--- auto_encoder.py
+++ auto_encoder.py
@@ -1,14 +1,20 @@
 from keras.models import Model
-from keras.layers import Input
+from keras.layers import Input, MaxPool2D
 
-from more_keras import ConvBlock
+from more_keras import ConvBlock, SEBlock, ResidualBlock
 
 
 class AutoEncoder(Model):
     def __init__(self, in_dim: int, in_features: int, n_features: int, blocks: int):
         x = inp = Input((in_dim, in_dim, in_features))
 
-        x = ConvBlock(32)(x)
+        x = ConvBlock(n_features)(x)
+
+        for n in range(1, blocks+1):
+            x = MaxPool2D()(x)
+            x = ConvBlock((2**n) * n_features)(x)
+            x = ConvBlock((2**n) * n_features)(x)
+            x = SEBlock()(x)
 
         super().__init__(inp, x)
 
--- more_keras/src/blocks/se_block.py
+++ more_keras/src/blocks/se_block.py
@@ -2,44 +2,22 @@ from keras.layers import Layer, Conv2D, BatchNormalization, Activation, Dense, G
 
 
 class SEBlock(Layer):
-    def __init__(self, n_features: int, activation: str = 'silu', kernel_size: int = 3, padding: str = 'same', **kwargs):
+    def __init__(self, activation: str = 'silu', **kwargs):
         super().__init__(**kwargs)
-        self.n_features = n_features
         self.activation = activation
-        self.kernel_size = kernel_size
-        self.padding = padding
 
     def build(self, input_shape):
-        self.conv1 = Conv2D(
-            self.n_features, self.kernel_size, padding=self.padding)
-        self.bn1 = BatchNormalization()
-        self.act1 = Activation(self.activation)
-
-        self.conv2 = Conv2D(
-            self.n_features, self.kernel_size, padding=self.padding)
-        self.bn2 = BatchNormalization()
-        self.act2 = Activation(self.activation)
-
+        features = input_shape[-1]
         self.squeeze = GlobalAveragePooling2D()
-        self.excite1 = Dense(self.n_features // 4, activation=self.activation)
-        self.excite2 = Dense(self.n_features, activation='sigmoid')
+        self.excite1 = Dense(features // 4, activation=self.activation)
+        self.excite2 = Dense(features, activation='sigmoid')
 
         return super().build(input_shape)
 
     def call(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.act1(x)
-
-        x = self.conv2(x)
-        x = self.bn2(x)
-
         squeeze = self.squeeze(x)
         excite = self.excite1(squeeze)
         excite = self.excite2(excite)
-        excite = Reshape((1, 1, self.n_features))(excite)
-
+        excite = Reshape((1, 1, -1))(excite)
         x *= excite
-        x = self.act2(x)
-
         return x
